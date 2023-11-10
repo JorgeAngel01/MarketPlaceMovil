018e0c0c9fadd29dbab6c692da85df095a223aaa
Angel <jorge.angelus0107@gmail.com>
2023-11-09 22:11:45 -0600

    file estructure

---
+++ src/theme/styles.js
@@ -0,0 +1,11 @@
+export const THEME = {
+    colors: {
+        primary: '#ff0000',
+        secondary: '#00ff00',
+    },
+    fonts: {
+        regular: 'Roboto, sans-serif',
+        medium: 'Roboto, sans-serif',
+        bold: 'Roboto, sans-serif',
+    },  
+};
