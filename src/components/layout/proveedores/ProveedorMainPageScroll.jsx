ffcf44c0e8f232900e4646a24c6d6d79fca4c9b5
NickABN <115517667+NicolasAlejandroBasilioNava@users.noreply.github.com>
2023-11-29 16:57:15 -0600

    Proveedores flatlist

--- src/components/layout/proveedores/ProveedorMainPageScroll.jsx
+++ src/components/layout/proveedores/ProveedorMainPageScroll.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { StyleSheet, View, ScrollView} from "react-native";
+import { FlatList} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import ProveedorCard from "./ProveedorCard";
 import { supplierData } from "../../../constants/testdata";
 
 const ProveedorMainPageScroll = ({proveedores}) => {
-  
+  const renderItem = ({ item }) => (
+    <ProveedorCard key={item.id} card={item} height={175} />
+  );
+
   return(
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={{ marginTop: 10, flex: 1, width: "100%" }}
-    >
-      {supplierData.map((card) => {
-        return <ProveedorCard key={card.id} card={card} height={175} />
-      })}
-    </ScrollView>
+    <FlatList
+    data={supplierData}
+    renderItem={renderItem}
+    keyExtractor={(item) => item.id.toString()}
+    showsVerticalScrollIndicator={false}
+    style={{ marginTop: 10, flex: 1, width: "100%" }}
+  />
   )
 }
 
